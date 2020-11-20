e25d7d873a69173f66390dbcb9f5b3447b2e93d2
Lulzx <wiserishabh@gmail.com>
2020-11-20 21:41:29 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Shoten Reader
+
+This is the repository for the reader and activity tracker for shoten application.
+
+## Written in Svelte with Typescript and TailwindCSS built with Snowpack
+
+Made by Lulzx
---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import TailwindCss from "./TailwindCSS.svelte";
+
+  import { onMount } from "svelte";
+
+  let count: number = 0;
+  onMount(() => {
+    const interval = setInterval(() => count++, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  });
+</script>
+
+time passed since eternity:
+{count}
