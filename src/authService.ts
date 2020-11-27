9a10a81c99f1fea78aadc2077d0f50d122f1c24b
Lulzx <wiserishabh@gmail.com>
2020-11-27 13:47:50 +0530

    make auth persistent across reloads

--- src/authService.ts
+++ src/authService.ts
@@ -1,45 +1,52 @@
-import createAuth0Client from "@auth0/auth0-spa-js";
-import {user, isAuthenticated, popupOpen} from "./store";
+import createAuth0Client from '@auth0/auth0-spa-js'
+import { user, isAuthenticated, popupOpen } from './store'
 
 const config = {
-  domain: "shoten.us.auth0.com",
-  client_id: "MG2ggKq3jrR0xZyzqLb2n9ScFQXD1kDX",
-  audience: "https://shoten.us.auth0.com/api/v2/",
-  callback_url: window.location.href,
+  domain: 'shoten.us.auth0.com',
+  client_id: 'MG2ggKq3jrR0xZyzqLb2n9ScFQXD1kDX',
+  audience: 'https://shoten.us.auth0.com/api/v2/',
+  redirect_uri: window.location.href,
   logout_url: window.location.href,
-}; 
+}
 
 async function createClient() {
-    let auth0Client = await createAuth0Client({
-        domain: config.domain,
-        client_id: config.client_id
-      });
+  let auth0Client = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.client_id,
+    cacheLocation: 'localstorage',
+  })
 
-      return auth0Client
+  return auth0Client
 }
 
-async function loginWithPopup(client: { loginWithPopup: (arg0: any) => any; getUser: () => {} | PromiseLike<{}>; }, options: any) {
-    popupOpen.set(true);
-    try {
-      await client.loginWithPopup(options);
-      user.set(await client.getUser());
-      isAuthenticated.set(true);
-    } catch (e) {
-      // eslint-disable-next-line
-      console.error(e);
-    } finally {
-        popupOpen.set(false);
-    }
+async function loginWithPopup(
+  client: {
+    loginWithPopup: (arg0: any) => any
+    getUser: () => {} | PromiseLike<{}>
+  },
+  options: any,
+) {
+  popupOpen.set(true)
+  try {
+    await client.loginWithPopup(options)
+    user.set(await client.getUser())
+    isAuthenticated.set(true)
+  } catch (e) {
+    // eslint-disable-next-line
+    console.error(e)
+  } finally {
+    popupOpen.set(false)
   }
+}
 
-  function logout(client: { logout: () => any; }) {
-    return client.logout();
-  }
+function logout(client: { logout: () => any }) {
+  return client.logout()
+}
 
 const auth = {
-    createClient,
-    loginWithPopup,
-    logout
+  createClient,
+  loginWithPopup,
+  logout,
 }
 
-export default auth;
+export default auth
